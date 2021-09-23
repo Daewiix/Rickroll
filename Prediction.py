585013e5b78e44006059a070386bd5a9c669292c
Naveen <90403761+Daewiix@users.noreply.github.com>
2021-09-23 17:58:11 +0530

    Add files via upload

---
+++ Prediction.py
@@ -0,0 +1,8 @@
+
+print( 'Hello!!,WE WILL BE PREDICTING HOW YOUR DAY WILL BE!!!') 
+input("What is your name?")
+input("Which month were you born in ?") 
+input("How many letters does your letter consist of?")
+input("Did you parents hit you today??")
+import webbrowser
+webbrowser.open("https://c.tenor.com/Z6gmDPeM6dgAAAAC/dance-moves.gif ")
